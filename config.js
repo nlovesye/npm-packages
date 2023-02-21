77eb91099254799e2e9b951f9a945d1d29149d2f
nlovesye <894765963@qq.com>
2023-02-21 10:54:19 +0800

    fix: config

--- config.js
+++ config.js
@@ -2,12 +2,16 @@ const path = require("path");
 
 const PORT = 10000;
 
-const HOME_DIR = process.env.HOME || "";
+const HOME_DIR = "E:/";
 
-console.log("HOME_DIR", HOME_DIR);
+const LIBRARY_ROOT = path.join(HOME_DIR, "/Videos");
+
+console.log("PORT: ", PORT);
+console.log("HOME_DIR: ", HOME_DIR);
+console.log("LIBRARY_ROOT: ", LIBRARY_ROOT);
 
 module.exports = {
     PORT,
     HOME_DIR,
-    LIBRARY_ROOT: path.join(HOME_DIR, "/Movies"),
+    LIBRARY_ROOT,
 };
