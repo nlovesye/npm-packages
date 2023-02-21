05758f8cd47b601cab9e360311fa3934061890c9
nlovesye <894765963@qq.com>
2023-02-21 18:04:48 +0800

    feat: 增加NODE_ENV环境变量

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
 const path = require("path");
 
+const NODE_ENV = process.env.NODE_ENV;
+
+const isPrd = "prd" === NODE_ENV;
+
 const PORT = 10000;
 
-const HOME_DIR = "E:/";
+const HOME_DIR = isPrd ? "E:/" : process.env.HOME;
 
-const LIBRARY_ROOT = path.join(HOME_DIR, "/VideoLibrary");
+const LIBRARY_ROOT = path.join(HOME_DIR, isPrd ? "VideoLibrary" : "Movies");
 
+console.log("NODE_ENV: ", process.env.NODE_ENV);
 console.log("PORT: ", PORT);
 console.log("HOME_DIR: ", HOME_DIR);
 console.log("LIBRARY_ROOT: ", LIBRARY_ROOT);
@@ -14,4 +19,5 @@ module.exports = {
     PORT,
     HOME_DIR,
     LIBRARY_ROOT,
+    NODE_ENV,
 };
