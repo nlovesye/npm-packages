11874b795adbaed69d82be1af874965c294643ba
nlovesye <894765963@qq.com>
2023-02-22 17:13:10 +0800

    feat: add sys env config

--- config.js
+++ config.js
@@ -2,25 +2,28 @@ const path = require("path");
 
 const NODE_ENV = process.env.NODE_ENV;
 
+const SYS = process.env.SYS;
+
 const isPrd = "prd" === NODE_ENV;
+
+const isMac = "mac" === SYS;
 
 const PORT = isPrd ? 10000 : 11000;
 
-const HOME_DIR = isPrd ? "E:/" : process.env.HOME;
+const HOME_DIR = !isMac ? "E:/" : process.env.HOME;
 
-const LIBRARY_ROOT = path.join(HOME_DIR, isPrd ? "VideoLibrary" : "Movies");
+const LIBRARY_ROOT = path.join(HOME_DIR, !isMac ? "VideoLibrary" : "Downloads/VideoLib");
 
 const WEB_STATIC = path.join(__dirname, "web");
 
 console.log("NODE_ENV: ", process.env.NODE_ENV);
+console.log("SYS: ", SYS);
 console.log("PORT: ", PORT);
-console.log("HOME_DIR: ", HOME_DIR);
 console.log("LIBRARY_ROOT: ", LIBRARY_ROOT);
 console.log("WEB_STATIC: ", WEB_STATIC);
 
 module.exports = {
     PORT,
-    HOME_DIR,
     LIBRARY_ROOT,
     NODE_ENV,
     WEB_STATIC,
