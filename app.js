7de4c72b6acf553799503fbb733b558a02f814f5
nlovesye <894765963@qq.com>
2023-02-22 10:15:26 +0800

    feat: 增加前端应用部署配置

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const logger = require("koa-logger");
 const cors = require("koa2-cors");
 const range = require("koa-range");
 const serveStatic = require("koa-static");
+const { historyApiFallback } = require("koa2-connect-history-api-fallback");
 
 const allRoutes = require("./routes");
-const { LIBRARY_ROOT } = require("./config");
+const { LIBRARY_ROOT, WEB_STATIC } = require("./config");
 
 const app = new Koa();
 
@@ -27,6 +28,12 @@ app.use(
 app.use(json());
 app.use(logger());
 app.use(range);
+
+// 此中间件需在静态服务器中间件前使用，whiteList（白名单）中的路由不会被指向到打包应用的**index.html**文件。
+app.use(historyApiFallback({ whiteList: ["/api"] }));
+// 前端应用 static
+app.use(serveStatic(WEB_STATIC));
+// static
 app.use(serveStatic(LIBRARY_ROOT));
 
 // logger
--- config.js
+++ config.js
@@ -4,20 +4,24 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const isPrd = "prd" === NODE_ENV;
 
-const PORT = 10000;
+const PORT = isPrd ? 10000 : 11000;
 
 const HOME_DIR = isPrd ? "E:/" : process.env.HOME;
 
 const LIBRARY_ROOT = path.join(HOME_DIR, isPrd ? "VideoLibrary" : "Movies");
 
+const WEB_STATIC = path.join(__dirname, "web");
+
 console.log("NODE_ENV: ", process.env.NODE_ENV);
 console.log("PORT: ", PORT);
 console.log("HOME_DIR: ", HOME_DIR);
 console.log("LIBRARY_ROOT: ", LIBRARY_ROOT);
+console.log("WEB_STATIC: ", WEB_STATIC);
 
 module.exports = {
     PORT,
     HOME_DIR,
     LIBRARY_ROOT,
     NODE_ENV,
+    WEB_STATIC,
 };
